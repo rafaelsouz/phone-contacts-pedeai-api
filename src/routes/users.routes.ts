224ddfe9436a5c9fc8ae73902700549cf3221903
Rafael Souza <raffaelsouza908@gmail.com>
2020-06-27 14:15:34 -0300

    Desacoplando repositório das rotas

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,16 @@
 import { Router } from 'express';
 
+import UsersRepository from '../repositories/UsersRepository';
+
 const usersRouter = Router();
+const usersRepository = new UsersRepository();
+
+usersRouter.post('/', (req, res) => {
+  const { name, email, password } = req.body;
+
+  const user = usersRepository.create({ name, email, password });
+
+  return res.json(user);
+});
 
 export default usersRouter;
