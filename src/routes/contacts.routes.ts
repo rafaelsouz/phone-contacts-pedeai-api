8cf2bede1b28fd8d46bbcb995911eb0a83f0bf1f
Rafael Souza <raffaelsouza908@gmail.com>
2020-06-28 16:52:34 -0300

    Chamando o service para criação de uma contato novo

--- src/routes/contacts.routes.ts
+++ src/routes/contacts.routes.ts
@@ -1,5 +1,17 @@
 import { Router } from 'express';
 
+import CreateContactService from '../services/CreateContactService';
+
 const contactsRouter = Router();
+
+contactsRouter.post('/', async (req, res) => {
+  const { name, email, phone, user_id } = req.body;
+
+  const createContact = new CreateContactService();
+
+  const contact = await createContact.execute({ name, email, phone, user_id });
+
+  return res.json(contact);
+});
 
 export default contactsRouter;
