a858d98011444db93c1938120bc2779ee3a1cfed
Rafael Souza <raffaelsouza908@gmail.com>
2020-06-28 15:10:53 -0300

    Aplicando decoretors e criando relacionamento N:1 com usuários.

---
+++ src/models/Contact.ts
@@ -0,0 +1,42 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+
+import User from './User';
+
+@Entity('contacts')
+class Contacts {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: 'user_id' })
+  user: User;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  phone: string;
+
+  @Column()
+  avatar: string;
+
+  @Column()
+  created_at: Date;
+
+  @Column()
+  updated_at: Date;
+}
+
+export default Contacts;
